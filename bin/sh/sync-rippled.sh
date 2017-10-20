b12e90c7074ffa7517ae868f175d35a395e27000
Edward Hennis <ed@ripple.com>
2017-10-20 19:30:04 -0400

    [WIP] Sync script

---
+++ bin/sh/sync-rippled.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# This script is a temporary workaround to keep rippled and ripple-libpp
+# source code in sync until that source is removed from rippled and
+# ripple-libpp becomes a dependency.
+
+set -e
+
+if [ $# == 1 ]
+then
+	rippled=$1
+else
+	echo "$(basename $0) <path to rippled source repo>" >&2
+	exit 1
+fi
+
+# Don't do anything if there are local changes
+git diff --exit-code
+
+
+# Build and test
+for dir in ./build/cmake/*
+do
+  cmake "${dir}"
+  cmake --build "${dir}"
+  "${dir}"/ripplelibppdemo.exe
+  "${dir}"/ripplelibpptest.exe --unittest
+done
+
+rsync -avP 
+
+echo Success
